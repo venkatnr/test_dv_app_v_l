f9e7587bb7c1605e2d7f7f67f880abdddc882723
venkatnr <oneguyvenkat007@gmail.com>
2012-09-06 17:31:48 +0530

    single line modification

--- app/controllers/iterations_controller.rb
+++ app/controllers/iterations_controller.rb
@@ -35,7 +35,7 @@ def create
     @project = Project.find(params[:project_id])
     @all_iterations = Iteration.find(:all, :select => "status" ,:conditions => {:status => "Open", :project_id => @project.id}).map(&:status).count
 	if @all_iterations.to_i >= 1
-		flash[:error] = "One record is already opened."
+		flash[:error] = "One record is already \"Opened\""
 		render :action => "new"
 	else  
 		@iteration = @project.iteration.create(params[:iteration])
